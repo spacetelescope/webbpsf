392a462c75d609662b73767e7808c01b6115933a
Marshall Perrin <mperrin@stsci.edu>
2013-10-30 19:38:41 -0400

    dev script updates

--- dev_utils/make-data-sdist.sh
+++ dev_utils/make-data-sdist.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Script to make a distributable version of WebbPSF, with various packaging tweaks
 
-VER="0.2.6"
+VER="0.3.0"
 TAR=/usr/bin/tar  # make sure to use the BSD version, required for the -L option
 
 
@@ -9,16 +9,17 @@ TAR=/usr/bin/tar  # make sure to use the BSD version, required for the -L option
 # make a copy of the filter file excluding FND to appease Gillian Wright, and exclude the FND from the tar file
 # Also exclude various things we don't want to distribute, like .svn, the old OPDs, and the data source directories
 
-mv data webbpsf-data
+cd ~/software/
+cp webbpsf-data/filters.txt webbpsf
 mv webbpsf-data/filters.txt .
 
 # remove FND, and set MIRI filters to the square profiles
 cat filters.txt | grep -v FND >  webbpsf-data/filters.txt
 # link to tophat filters
-\rm webbpsf-data/MIRI/filters
+\rm -r webbpsf-data/MIRI/filters
 ln -s tophat_filters webbpsf-data/MIRI/filters
 # create public distributable tar file
-$TAR -cvz -L  --exclude .svn --exclude OPD_RevT --exclude .DS_Store --exclude sources --exclude "*FND*" --exclude "*_filters" -f webbpsf-data-public-$VER.tar.gz webbpsf-data
+$TAR -cvz -L  --exclude .svn --exclude OPD_RevT --exclude TFI --exclude .DS_Store --exclude sources --exclude "*FND*" --exclude "*_filters" -f webbpsf/dist/webbpsf-data-public-$VER.tar.gz webbpsf-data
 
 
 # Make a copy with more real data, for internal or CoroWG use
@@ -28,11 +29,10 @@ mv filters.txt webbpsf-data/filters.txt
 # link to measured filters
 \rm webbpsf-data/MIRI/filters
 ln -s measured_filters webbpsf-data/MIRI/filters
-# create public distributable tar file
-$TAR -cvz -L  --exclude .svn --exclude OPD_RevT --exclude .DS_Store --exclude sources -f webbpsf-data-internal-$VER.tar.gz webbpsf-data
+# create internal distributable tar file
+$TAR -cvz -L  --exclude .svn --exclude OPD_RevT --exclude TFI --exclude .DS_Store -f webbpsf/dist/webbpsf-data-internal-$VER.tar.gz webbpsf-data
 
 
 
-mv  webbpsf-data data
-\cp webbpsf-data-public-$VER.tar.gz ~/web/software/webbpsf/webbpsf-data-$VER.tar.gz
+\cp webbpsf/dist/webbpsf-data-public-$VER.tar.gz ~/web/software/webbpsf/webbpsf-data-$VER.tar.gz
 
