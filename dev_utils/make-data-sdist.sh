f6e2efb82665c156fdd3e9fe7f6cf4c95be332b8
Joseph Long <jdl@fastmail.fm>
2015-06-30 23:11:58 -0400

    Revisions to make-data-sdist.sh

--- dev_utils/make-data-sdist.sh
+++ dev_utils/make-data-sdist.sh
@@ -1,12 +1,31 @@
 #!/bin/sh
 # Script to make a distributable version of WebbPSF, with various packaging tweaks
+set -e
 
-VER="0.3.1"
-TAR=/usr/bin/tar  # make sure to use the BSD version, required for the -L option
-DATAROOT=/itar/jwst/tel/share/webbpsf/webbpsf-data-source
+if [ -z VER ]; then
+  echo "Provide a version: VER=\"0.3.3\" ./make-data-sdist.sh"
+  exit 1
+fi
 
-export COPYFILE_DISABLE=1 	# If on Mac OS, tell tar to not include ._* files for
-		   	# HFS-specific extended attributes
+if [ -z DATAROOT ]; then
+  DATAROOT=/itar/jwst/tel/share/webbpsf/webbpsf-data-source
+fi
+
+if [ -z DEST ]; then
+  DEST="$(pwd)"
+fi
+
+# If on Mac OS, tell tar to not include ._* files for
+# HFS-specific extended attributes
+export COPYFILE_DISABLE=1
+
+TMPDIR="/tmp/webbpsf-data"
+
+mkdir $TMPDIR
+rsync -avz "$DATAROOT" $TMPDIR
+
+# Remove existing ._* files
+find $TMPDIR -name "._*" -exec rm -i {} \;
 
 # Create the data tarfile
 # make a copy of the filter file excluding FND to appease MIRI PI requirements
@@ -15,37 +34,34 @@ export COPYFILE_DISABLE=1 	# If on Mac OS, tell tar to not include ._* files for
 # set MIRI filters to the square profiles for broad distribution
 # link to tophat filters
 echo "Setting up for simplified MIRI filter profiles"
-\rm -r $DATAROOT/MIRI/filters
-ln -s $DATAROOT/MIRI/tophat_filters $DATAROOT/MIRI/filters
+\rm $TMPDIR/MIRI/filters
+ln -s $TMPDIR/MIRI/tophat_filters $TMPDIR/MIRI/filters
 
 
 # create public distributable tar file
-$TAR -cvz -L -C $DATAROOT/..  \
+tar -cvz -L -C $TMPDIR/..  \
     --exclude .svn --exclude OPD_RevT --exclude TFI --exclude .DS_Store \
     --exclude sources --exclude "*FND*" --exclude "*_filters" --exclude "*py" \
     --exclude "_Obsolete" --exclude README_DEVEL.txt \
-    -s "/webbpsf-data-source/webbpsf-data/" \
-    -f webbpsf-data-$VER.tar.gz webbpsf-data-source
+    -f "webbpsf-data-$VER.tar.gz" webbpsf-data
 
+cp "$TMPDIR/webbpsf-data-$VER.tar.gz" "$DEST/"
 
 # Make a copy with the complete MIRI filter profiles, for internal or CoroWG use
 
 # link to measured filters
 echo "Setting up for measured MIRI filter profiles"
-\rm $DATAROOT/MIRI/filters
-ln -s $DATAROOT/MIRI/measured_filters $DATAROOT/MIRI/filters
+\rm $TMPDIR/MIRI/filters
+ln -s $TMPDIR/MIRI/measured_filters $TMPDIR/MIRI/filters
 # creat internal distributable tar file
-$TAR -cvz -L -C $DATAROOT/..  \
+tar -cvz -L -C $TMPDIR/..  \
     --exclude .svn --exclude OPD_RevT --exclude TFI --exclude .DS_Store \
     --exclude "*_filters" --exclude "*py" \
     --exclude "_Obsolete" --exclude README_DEVEL.txt \
-    -s "/webbpsf-data-source/webbpsf-data/"  \
-    -f webbpsf-data-internal-$VER.tar.gz webbpsf-data-source
+    -f "webbpsf-data-internal-$VER.tar.gz" webbpsf-data-source
 
+cp "$TMPDIR/webbpsf-data-internal-$VER.tar.gz" "$DEST/"
 
-
-#\cp webbpsf/dist/webbpsf-data-public-$VER.tar.gz ~/web/software/webbpsf/webbpsf-data-$VER.tar.gz
-
-echo "Public file output to:    $PWD/webbpsf-data-$VER.tar.gz"
-echo "Internal file output to:  $PWD/webbpsf-data-internal-$VER.tar.gz"
+echo "Public file output to:    $DEST/webbpsf-data-$VER.tar.gz"
+echo "Internal file output to:  $DEST/webbpsf-data-internal-$VER.tar.gz"
 
