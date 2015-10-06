6d491410ed05eff6c132bea82d3954ad85493b0e
Joseph Long <jlong@stsci.edu>
2015-10-06 10:46:22 -0400

    Use round() instead of int() to get correct snapping to integers in build_detector_from_table

--- webbpsf/wfirst.py
+++ webbpsf/wfirst.py
@@ -190,7 +190,8 @@ def _load_wfi_detector_aberrations(filename):
             # (local_x in mm / 10 um pixel size) -> * 1e2
             # local_x and _y range from -20.44 to +20.44, so adding to the midpoint pixel
             # makes sense to place (-20.44, -20.44) at (4, 4)
-            pixx, pixy = int(midpoint_pixel + local_x * 1e2), int(midpoint_pixel + local_y * 1e2)
+            pixx, pixy = (round(midpoint_pixel + local_x * 1e2),
+                          round(midpoint_pixel + local_y * 1e2))
 
             detector.add_field_point(pixx, pixy, interpolator)
         return detector
