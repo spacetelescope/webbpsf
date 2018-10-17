5c50b779bf155d684605c038b30ee498ea68498c
Shannon Osborne <sosborne@stsci.edu>
2018-10-17 12:13:33 -0400

    Adding documentation to tests

--- webbpsf/tests/test_psfgrid.py
+++ webbpsf/tests/test_psfgrid.py
@@ -10,98 +10,72 @@ from .. import webbpsf_core
 
 # @pytest.mark.skip()
 def test_compare_to_calc_psf_oversampled():
-    """Check that the output PSF matches calc_psf and is saved in the correct location:
+    """Check that the output PSF matches calc_psf and is saved in the correct slice of the array:
     for a distorted, oversampled case"""
     oversample = 2
     fov_pixels = 11
 
+    # Create PSF grid
     fgs = webbpsf_core.FGS()
     fgs.detector = "FGS1"
     grid = fgs.psf_grid(all_detectors=False, num_psfs=4, oversample=oversample, fov_pixels=fov_pixels)
 
+    # Pull one of the PSFs out of the grid
     psfnum = 1
     loc = grid[0].header["DET_YX{}".format(psfnum)]
     locy = int(loc.split()[0][1:-1])
     locx = int(loc.split()[1][:-1])
     gridpsf = grid[0].data[psfnum, :, :]
 
+    # Using header data, create the expected same PSF via calc_psf
     fgs.detector_position = (locx, locy)
     calcpsf = fgs.calc_psf(oversample=oversample, fov_pixels=fov_pixels)["OVERDIST"].data
     kernel = astropy.convolution.Box2DKernel(width=oversample)
     convpsf = astropy.convolution.convolve(calcpsf, kernel)
 
+    # Compare to make sure they are in fact the same PSF
     assert gridpsf.shape == calcpsf.shape
     assert np.array_equal(gridpsf, convpsf)
 
 
 # @pytest.mark.skip()
 def test_comapre_to_calc_psf_detsampled():
-    """Check that the output PSF matches calc_psf and is saved in the correct location:
+    """Check that the output PSF matches calc_psf and is saved in the correct slice of the array:
     for an un-distorted, detector sampled case"""
     oversample = 2
     fov_arcsec = 0.5
 
+    # Create PSF grid
     mir = webbpsf_core.MIRI()
     mir.filter = "F560W"
     mir.detector = "MIRIM"
     grid = mir.psf_grid(all_detectors=False, num_psfs=4, use_detsampled_psf=True, add_distortion=False,
                        oversample=oversample, fov_arcsec=fov_arcsec)
 
+    # Pull one of the PSFs out of the grid
     psfnum = 1
     loc = grid[0].header["DET_YX{}".format(psfnum)]
     locy = int(loc.split()[0][1:-1])
     locx = int(loc.split()[1][:-1])
     gridpsf = grid[0].data[psfnum, :, :]
 
+    # Using header data, create the expected same PSF via calc_psf
     mir.detector_position = (locx, locy)
     mir.options['output_mode'] = 'Detector Sampled Image'
     calcpsf = mir.calc_psf(oversample=oversample, fov_arcsec=fov_arcsec)["DET_SAMP"].data
     kernel = astropy.convolution.Box2DKernel(width=1)
     convpsf = astropy.convolution.convolve(calcpsf, kernel)
 
+    # Compare to make sure they are in fact the same PSF
     assert gridpsf.shape == calcpsf.shape
     assert np.array_equal(gridpsf, convpsf)
 
 
 # @pytest.mark.skip()
 def test_all():
-    """Check that running all the detectors works"""
-    fgs = webbpsf_core.FGS()
-    grid = fgs.psf_grid(all_detectors=True, num_psfs=4, fov_pixels=10, oversample=2)
-
-    # Check they shape
-    assert len(grid) == 2
-    assert grid[0][0].data.shape == (4, 20, 20)
-
-
-# @pytest.mark.skip()
-def test_one_psf():
-    """Check that setting num_psfs = 1 produces the PSF in the right location"""
-    oversample = 2
-    fov_pixels = 11
-
-    nis = webbpsf_core.NIRISS()
-    nis.filter = "F140M"
-    grid1 = nis.psf_grid(all_detectors=False, num_psfs=1, add_distortion=True, oversample=oversample,
-                         fov_pixels=fov_pixels, single_psf_centered=True, use_detsampled_psf=False)
-
-    nis.detector_position = (10, 0)  # it's set as (x,y)
-    grid2 = nis.psf_grid(all_detectors=False, num_psfs=1, add_distortion=True, oversample=oversample,
-                         fov_pixels=fov_pixels, single_psf_centered=False, use_detsampled_psf=False)
-
-    calc = nis.calc_psf(add_distortion=True, oversample=2, fov_pixels=11)
-    kernel = astropy.convolution.Box2DKernel(width=oversample)
-    conv = astropy.convolution.convolve(calc["OVERDIST"].data, kernel)
-
-    assert grid1[0].header["DET_YX0"] == "(1024, 1024)"  # the default is the center of the NIS aperture
-    assert grid2[0].header["DET_YX0"] == "(0, 10)"  # it's in (y,x)
-    assert np.array_equal(conv, grid2[0].data[0, :, :])
-
-
-# @pytest.mark.skip()
-def test_nircam_all_detectors():
-    """Test that the NIRCam detectors pulled are correct (shortwave vs longwave)
-    with respect to the filter when all_detectors=True"""
+    """Check that running all the detectors works (ie setting all_detectors=True). In
+    particular for NIRCam, test that the detectors pulled are correct
+    (shortwave vs longwave) with respect to the filter"""
 
     nir = webbpsf_core.NIRCam()
     longfilt = "F250M"
@@ -129,8 +103,36 @@ def test_nircam_all_detectors():
 
 
 # @pytest.mark.skip()
+def test_one_psf():
+    """Check that setting num_psfs = 1 produces the PSF in the right location"""
+    oversample = 2
+    fov_pixels = 11
+
+    nis = webbpsf_core.NIRISS()
+    nis.filter = "F140M"
+
+    # Case 1: The PSF is centered on the detector (with single_psf_centered=True)
+    grid1 = nis.psf_grid(all_detectors=False, num_psfs=1, add_distortion=True, oversample=oversample,
+                         fov_pixels=fov_pixels, single_psf_centered=True, use_detsampled_psf=False)
+
+    # Case 2: The PSF is set to a specific position (with nis.detector_position = (10, 0))
+    nis.detector_position = (10, 0)  # it's set as (x,y)
+    grid2 = nis.psf_grid(all_detectors=False, num_psfs=1, add_distortion=True, oversample=oversample,
+                         fov_pixels=fov_pixels, single_psf_centered=False, use_detsampled_psf=False)
+
+    # Compare Case 2 to the calc_psf output to make sure it's placing the PSF in the right location
+    calc = nis.calc_psf(add_distortion=True, oversample=2, fov_pixels=11)
+    kernel = astropy.convolution.Box2DKernel(width=oversample)
+    convpsf = astropy.convolution.convolve(calc["OVERDIST"].data, kernel)
+
+    assert grid1[0].header["DET_YX0"] == "(1024, 1024)"  # the default is the center of the NIS aperture
+    assert grid2[0].header["DET_YX0"] == "(0, 10)"  # it's in (y,x)
+    assert np.array_equal(convpsf, grid2[0].data[0, :, :])
+
+
+# @pytest.mark.skip()
 def test_nircam_errors():
-    """Check that NIRCam has checks for incorrect value setting"""
+    """Check that there are checks for incorrect value setting - particularly with NIRCam"""
     longfilt = "F250M"
     shortfilt = "F140M"
     longdet = "NRCB5"
@@ -138,7 +140,7 @@ def test_nircam_errors():
 
     nir = webbpsf_core.NIRCam()
 
-    # Shouldn't error
+    # Shouldn't error - applying SW to SW and LW to LW
     nir.filter = longfilt
     nir.detector = longdet
     nir.psf_grid(all_detectors=False, num_psfs=1, fov_pixels=1, detector_oversample=2, fft_oversample=2)   # no error
@@ -147,38 +149,40 @@ def test_nircam_errors():
     nir.detector = shortdet
     nir.psf_grid(all_detectors=False, num_psfs=1, fov_pixels=1, detector_oversample=2, fft_oversample=2) # no error
 
-    # Should error
+    # Should error - Bad filter/detector combination (LW filt to SW det)
     with pytest.raises(ValueError) as excinfo:
-        # Bad filter/detector combination
         nir.filter =longfilt
         nir.detector =shortdet
         nir.psf_grid(all_detectors=False, num_psfs=1, fov_pixels=1)  # error
     assert "ValueError" in str(excinfo)
 
+    # Should error - Bad filter/detector combination (SW filt to LW det)
     with pytest.raises(ValueError) as excinfo:
-        # Bad filter/detector combination
         nir.filter =shortfilt
         nir.detector =longdet
         nir.psf_grid(all_detectors=False, num_psfs=1, fov_pixels=1)  # error
     assert "ValueError" in str(excinfo)
 
+    # Should error - Bad num_psfs entry (must be a square number)
     with pytest.raises(ValueError) as excinfo:
-        # Bad num_psfs entry
         nir.psf_grid(all_detectors=False, num_psfs=2, fov_pixels=1)  # error
     assert "ValueError" in str(excinfo)
 
+
 # @pytest.mark.skip()
 def test_saving(tmpdir):
-    """Test saving files"""
+    """Test saving files works properly"""
 
+    # Create a temp directory to place file in
     file = str(tmpdir.join("test1"))
+
+    # Test using default calc_psf values
     fgs = webbpsf_core.FGS()
     fgs.filter = "FGS"
     fgs.detector = "FGS2"
-
-    # Using default calc_psf values
     grid = fgs.psf_grid(all_detectors=False, num_psfs=4, save=True, outfile=file, overwrite=True)
 
+    # Check that the saved file matches the returned file (and thus that the save worked through properly)
     with fits.open(os.path.join(file[:-5], "test1_fgs2_fgs.fits")) as infile:
         assert infile[0].header == grid[0].header
         assert np.array_equal(infile[0].data, grid[0].data)
